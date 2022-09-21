6540e0f8461711078985afc41f745cd73ebf036a
Jimmy Yen <cy.yen1988@gmail.com>
2022-09-21 16:07:25 +0800

    commit

--- components/BurgerMenu.vue
+++ components/BurgerMenu.vue
@@ -10,10 +10,7 @@
       <ul class="text-white text-[5.093vw] text-center leading-[15vw]">
         <li @click="link('/assist')">首页</li>
         <li @click="link('/announcement')">公告中心</li>
-        <li @click="link('/question')">交易常见问题</li>
-        <li @click="link('/guide')">新手指南</li>
         <li @click="link('/agreement')">协议条款</li>
-        <li @click="link('/operation')">运营活动</li>
       </ul>
     </div>
   </div>
