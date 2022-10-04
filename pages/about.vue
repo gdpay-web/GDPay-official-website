b108d054126b9e831d36d35185f0ea4c44b65f3c
Zuray David <intheblackworld@gmail.com>
2022-10-04 14:48:31 +0800

    關於我們文案更新

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="flex-c-c flex-col text-[3.333vw] text-[#666666] py-[45.222vw] xl:text-[25px] xl:py-[200px]">
-    <img src="images/triangle-exclamation-solid.png" class="w-[19.074vw] xl:w-[144px]" alt="" />
-    <p class="mt-[6.667vw] xl:mt-[50px]">暂无内容</p>
+  <div class="flex-c-c flex-col text-[3.333vw] text-[#666666] xl:text-[25px] pt-[50px] pb-[200px] xl:w-[960px] xl:m-auto px-[5vw] xl:px-0">
+    <!-- <img src="images/triangle-exclamation-solid.png" class="w-[19.074vw] xl:w-[144px]" alt="" />
+    <p class="mt-[6.667vw] xl:mt-[50px]">暂无内容</p> -->
+
+    <p class="xl:mt-[50px] text-[16px] xl:text-[24px] leading-[1.7]">GDpay跨境加密货币金流服务，一个引领金融的钱包。致力于为全球用户提供最安全的支付和资产管理服务。极致体验 安全高效 持续为客户创造更多价值...
+    </p>
+    <p class="mt-[6.667vw] xl:mt-[50px] leading-[1.7] text-[16px] xl:text-[18px]"> GDpay是以千万级用户的商家为基础，为上万个客户提供安全、灵活、快速的数字支付业务。安全无风险，支付新趋势！<br />
+      ☆客户对客户一对一自由交易，用时短，速度快 ，可采用支付宝，微信，银行卡等进行交易 ！<br />
+      ☆支付稳定，大额无忧，拥有业内顶尖的安全团队，搭建了完善的风险控制架构，确保账户安全,交易更放心！<br />
+      ☆不受时间及国界限制，无论何时何地都能完成交易安全高效,无需远行即可畅玩全球!<br />
+      ☆汇率稳定性高，恒定人民币币值1:1 100%互兑 !<br />
+      ❤❤❤7x24h客服在线，随时响应客户需求!
+    </p>
   </div>
 </template>
 <script>
