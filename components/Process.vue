34a188d593c817d7db71a444cc2741644c9061d9
Zuray David <intheblackworld@gmail.com>
2022-12-02 17:14:27 +0800

    test action

--- components/Process.vue
+++ components/Process.vue
@@ -3,7 +3,7 @@
     <h3 class="text-[4.444vw] text-blue font-bold xl:text-[24px]">
       {{ title }}
     </h3>
-    <img src="images/A.png" class="w-[7.13vw] xl:w-[40px] -rotate-90" alt="" />
+    <img src="images/A.png" class="mt-[4vw] xl:mt-[0px] w-[7.13vw] xl:w-[40px] -rotate-90" alt="" />
   </div>
 </template>
 
